73c9d2670f955a387d8feb45d23e30e56706b3aa
Jason Chi <jason.chi52@gmail.com>
2025-11-02 22:53:08 -0500

    still fixing

--- terraform/network.tf
+++ terraform/network.tf
@@ -14,10 +14,15 @@ data "aws_subnets" "default" {
 # DB Subnet Group 
 
 resource "aws_db_subnet_group" "stock_news_analyzer_db_subnet_group" {
-  name       = "stock_news_analyzer_db_subnet_group"
+
+  name       = "stock_news_analyzer_db_subnet_group_default"
   subnet_ids = slice(data.aws_subnets.default.ids, 0, 2)
 
   tags = {
-    Name = "Stock News Analyzer DB Subnet Group"
+    Name = "Stock News Analyzer DB Subnet Group (default VPC)"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
