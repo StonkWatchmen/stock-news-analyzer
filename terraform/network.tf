ea9f7840c9b67d9b8bb84dbd3bb097f91fce0ef9
Jason Chi <jason.chi52@gmail.com>
2025-11-04 12:21:44 -0500

    changed subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -15,7 +15,11 @@ data "aws_subnets" "default" {
 
 resource "aws_db_subnet_group" "stock_news_analyzer_db_subnet_group" {
   name       = "stock_news_analyzer_db_subnet_group_default"
-  subnet_ids = slice(data.aws_subnets.default.ids, 0, 2)
+  subnet_ids = slice(
+    data.aws_subnets.default.ids,
+    0,
+    min(2, length(data.aws_subnets.default.ids))
+  )
 
   tags = {
     Name = "Stock News Analyzer DB Subnet Group - default VPC"
