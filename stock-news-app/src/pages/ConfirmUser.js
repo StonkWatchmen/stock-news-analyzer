0daee42b706af331736da4972c83fa408df97528
Anthony Lieu <anthonyjunlieu@gmail.com>
2025-11-04 12:48:42 -0500

    DEBUG LINK WARNING AS ERROR"

--- stock-news-app/src/pages/ConfirmUser.js
+++ stock-news-app/src/pages/ConfirmUser.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { confirmUser, resendConfirmation } from "../Cognito";
 import "./Auth.css";
 
