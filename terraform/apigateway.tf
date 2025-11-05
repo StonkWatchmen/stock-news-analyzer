6fef56327407febd31c7e5bc72c598c8b2a73f94
Anthony Lieu <anthonyjunlieu@gmail.com>
2025-11-05 17:55:16 -0500

    add lambda integration to api gateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -97,3 +97,23 @@ resource "aws_lambda_function" "get_stocks_lambda" {
     security_group_ids = [aws_security_group.lambda_sg.id]
   }
 }
+
+resource "aws_api_gateway_integration" "get_stocks_lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.stock-news-analyzer-api.id
+  resource_id = aws_api_gateway_resource.stocks.id
+  http_method = aws_api_gateway_method.get_stocks.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_stocks_lambda.invoke_arn
+
+  depends_on = [aws_lambda_function.get_stocks_lambda]
+}
+
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_stocks_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.stock-news-analyzer-api.execution_arn}/*/*"
+}
