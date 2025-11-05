ed03691563ec3411db092616fe03a321f96a1a0b
Anthony Lieu <anthonyjunlieu@gmail.com>
2025-11-05 16:27:06 -0500

    debug

--- terraform/security.tf
+++ terraform/security.tf
@@ -43,13 +43,6 @@ resource "aws_security_group" "lambda_sg" {
     cidr_blocks = ["0.0.0.0/0"]  
   }
 
-  ingress {
-    from_port   = 3306           
-    to_port     = 3306
-    protocol    = "tcp"
-    security_groups = [aws_security_group.rds_sg.id]
-  }
-
   tags = {
     Name = "stock-news-analyzer-lambda-sg"
   }
