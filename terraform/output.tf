d854c30b446c595f6f5e0545c44f57f44c9e484c
Jason Chi <jason.chi52@gmail.com>
2025-11-10 04:31:31 -0500

    small mistake fixed on destory

--- terraform/output.tf
+++ terraform/output.tf
@@ -13,3 +13,10 @@ output "cognito_client_id" {
 output "cognito_domain" {
   value = aws_cognito_user_pool_domain.auth_domain.domain
 }
+
+
+output "api_base_url" {
+  value       = aws_api_gateway_deployment.stock-news-analyzer-deployment.invoke_url
+  description = "API base URL (stage)"
+}
+
