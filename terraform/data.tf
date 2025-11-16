f5cc3eed71292ae223ce19e7bf6b5070e607e8db
Anthony Guo <ayg9231@rit.edu>
2025-11-16 18:24:35 -0500

    new zip for adding user

--- terraform/data.tf
+++ terraform/data.tf
@@ -59,3 +59,10 @@ data "archive_file" "init_rds_zip" {
   source_dir  = "${path.module}/build/init_rds"
   output_path = "${path.module}/init_rds.zip"
 }
+
+data "archive_file" "add_user_zip" {
+  depends_on  = [null_resource.package_lambda_add_user]
+  type        = "zip"
+  source_dir  = "${path.module}/build/add_user"
+  output_path = "${path.module}/add_user.zip"
+}
