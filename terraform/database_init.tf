e4003611a38846f917c2a8a96245277a325bbd0b
Jason Chi <jason.chi52@gmail.com>
2025-11-18 21:08:03 -0500

    lambnda initiazing rds

---
+++ terraform/database_init.tf
@@ -0,0 +1,19 @@
+# Lambda invocation to initialize database
+resource "aws_lambda_invocation" "init_database" {
+  function_name = aws_lambda_function.init_db_lambda.function_name
+
+  input = jsonencode({
+    action = "initialize_database"
+  })
+
+  depends_on = [
+    aws_db_instance.stock_news_analyzer_db,
+    aws_lambda_function.init_db_lambda
+  ]
+
+  lifecycle {
+    replace_triggered_by = [
+      aws_db_instance.stock_news_analyzer_db
+    ]
+  }
+}
