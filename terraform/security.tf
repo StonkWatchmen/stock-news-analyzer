18a80edccb1b9c5001bc8e0fc8ad778f75efd0db
Jason Chi <jason.chi52@gmail.com>
2025-11-18 23:03:05 -0500

    fix for long destory?

--- terraform/security.tf
+++ terraform/security.tf
@@ -5,23 +5,11 @@ resource "aws_security_group" "rds_sg" {
   vpc_id      = aws_vpc.stock_news_analyzer_vpc.id
 
   ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id, aws_security_group.lambda_sg.id]
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ec2_sg" {
-  name   = "stock_news_analyzer_ec2_sg"
-  vpc_id = aws_vpc.stock_news_analyzer_vpc.id
 
   egress {
     from_port   = 0
