2303882a2039076cda405dee87b6f05b70f9f7da
DannyCato <dg@catorcini.com>
2025-11-19 06:50:09 -0500

    removing my info for testing purposes

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -2,11 +2,11 @@ resource "aws_sns_topic" "notifications" {
   name = "hourly-updates"
 }
 
-resource "aws_sns_topic_subscription" "user" {
-  topic_arn = aws_sns_topic.notifications.arn
-  protocol  = "email"
-  endpoint  = "dg@catorcini.com"
-}
+# resource "aws_sns_topic_subscription" "user" {
+#   topic_arn = aws_sns_topic.notifications.arn
+#   protocol  = "email"
+#   endpoint  = "dg@catorcini.com"
+# }
 
 resource "aws_lambda_function" "attach_notifs_lambda" {
   function_name = "attach-notifs-lambda"
