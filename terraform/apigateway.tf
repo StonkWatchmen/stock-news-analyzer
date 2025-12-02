85f9be9b62d3a59120f3206803ee76331ca9495b
Anthony Lieu <anthonyjunlieu@gmail.com>
2025-12-02 14:10:34 -0500

    debug api gateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -46,9 +46,7 @@ resource "aws_api_gateway_method" "get_stocks" {
   rest_api_id   = aws_api_gateway_rest_api.stock-news-analyzer-api.id
   resource_id   = aws_api_gateway_resource.stocks.id
   http_method   = "GET"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
-  depends_on    = [aws_api_gateway_authorizer.cognito_authorizer]
+  authorization = "NONE"  # Changed from "COGNITO_USER_POOLS" - stocks list should be public
 }
 
 resource "aws_api_gateway_method" "stocks_options" {
